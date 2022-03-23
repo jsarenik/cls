a41bd794e1fadee336cf3c9f9f552fb183cd85e1
Jan Sarenik <jajomojo@gmail.com>
2022-03-23 13:39:29 +0100

    payzero.sh+payabit.sh: case-insensitive

--- payabit.sh
+++ payabit.sh
@@ -2,8 +2,8 @@
 
 ADD="bolt11=$1"
 test "$2" = "" || {
-  echo $1 | grep ^lnbc && ADD="bolt11=$1 msatoshi=$2"
-  echo $2 | grep ^lnbc && ADD="bolt11=$2 msatoshi=$1"
+  echo $1 | grep -i ^lnbc && ADD="bolt11=$1 msatoshi=$2"
+  echo $2 | grep -i ^lnbc && ADD="bolt11=$2 msatoshi=$1"
 }
 lch.sh -k pay \
   maxfeepercent=1 \
