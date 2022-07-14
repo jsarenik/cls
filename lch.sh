8559b3fcdc8073a3e6dacaa31b55b6ba4165a5b4
Jan Sarenik <jajomojo@gmail.com>
2022-07-14 18:56:24 +0200

    lch.sh: Make it run also from .lightning/bitcoin

--- lch.sh
+++ lch.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+pwd | grep -q '/bitcoin$' && cd ..
 test -d $PWD/bitcoin \
   || for net in testnet signet regtest
      do test "${PWD##*/}" = "$net" && { D="$PWD/.."; O="--$net"; break; }
