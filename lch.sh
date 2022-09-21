45a4bc979c332ea13b47aa4c4c6b2365e85b16f1
Jan Sarenik <jajomojo@gmail.com>
2022-09-21 14:52:54 +0200

    lch.sh: Use --network as well

--- lch.sh
+++ lch.sh
@@ -3,7 +3,12 @@
 pwd | grep -q '/bitcoin$' && cd ..
 test -d $PWD/bitcoin \
   || for net in testnet signet regtest
-     do test "${PWD##*/}" = "$net" && { D="$PWD/.."; O="--$net"; break; }
+     do
+       test "${PWD##*/}" = "$net" && {
+         D="$PWD/..";
+         O="--network $net";
+         break;
+       }
      done
 
 exec timeout 600 lightning-cli "--lightning-dir=${D:-$PWD}" $O "$@"
