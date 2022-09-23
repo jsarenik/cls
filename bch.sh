042f3a53f30cc7f36725f1ef5f9c4dd8e73b35e8
Jan Sarenik <jajomojo@gmail.com>
2022-09-23 09:55:31 +0200

    bch.sh: No variables, no fallback
    Fallback to ~/.bitcoin removed as it may be silly.

--- bch.sh
+++ bch.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-for net in testnet3 signet regtest
+for net in signet regtest testnet3
 do
-  test -d $net && break
-  test "${PWD##*/}" = "$net" && { D="$PWD/.."; O="-${net%3}"; break; }
+  test "${PWD##*/}" = "$net" && {
+    exec bitcoin-cli "-datadir=${PWD%/*}" "-${net%3}" "$@"
+  }
 done
-test -d chainstate || cd ~/.bitcoin
 
-exec bitcoin-cli "-datadir=${D:-$PWD}" $O "$@"
+exec bitcoin-cli "-datadir=$PWD" "$@"
