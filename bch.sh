cad955f2ae738199892eeb48661c9fe142925266
Jan Sarenik <jajomojo@gmail.com>
2022-09-23 08:43:49 +0200

    bch.sh: Add fallback to ~/.bitcoin

--- bch.sh
+++ bch.sh
@@ -5,5 +5,6 @@ do
   test -d $net && break
   test "${PWD##*/}" = "$net" && { D="$PWD/.."; O="-${net%3}"; break; }
 done
+test -d chainstate || cd ~/.bitcoin
 
 exec bitcoin-cli "-datadir=${D:-$PWD}" $O "$@"
