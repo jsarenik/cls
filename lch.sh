e463f48435e06bf4968685853e67167eaf93033b
Jan Sarenik <jajomojo@gmail.com>
2022-10-13 08:57:18 +0200

    Remove -V, collides

--- lch.sh
+++ lch.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-test "$1" = "-V" && {
-  VER=1.0.1
-  echo ${0##*/}-$VER-$(sed 1d $0 | md5sum | cut -b-5)
-  exit
-}
-
 test -d "$1" && { cd "$1"; shift; }
 
 test -d $PWD/bitcoin && cd bitcoin
