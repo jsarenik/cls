29230118ba1da6fa3fb08e6f4921d72ac95c2a76
Jan Sarenik <jajomojo@gmail.com>
2023-01-10 16:02:10 +0100

    b*h.sh: exec only once
    The magic is all in shell, one level removed - easier to
    include as a function in scripts. Functions examples will
    be added in next commit.

--- bdh.sh
+++ bdh.sh
@@ -4,9 +4,8 @@ test -d "$1" && { cd "$1"; shift; }
 
 for net in signet regtest testnet3
 do
-  test "${PWD##*/}" = "$net" && {
-    exec bitcoind "-datadir=${PWD%/*}" "-chain=${net%net3}" "$@"
-  }
+  test "${PWD##*/}" = "$net" || continue
+  chain=${net%net3}; ddir=${PWD%/*}
 done
 
-exec bitcoind "-datadir=$PWD" -chain=main "$@"
+exec bitcoind "-datadir=${ddir:-$PWD}" -chain=${chain:-main} "$@"
