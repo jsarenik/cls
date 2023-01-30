66484d2c126055b7a375e17cd130596c9243d96c
Jan Sarenik <jajomojo@gmail.com>
2023-01-30 20:47:54 +0100

    bfunky.sh: Add flattened lch and ldh

--- bfunky.sh
+++ bfunky.sh
@@ -3,11 +3,9 @@
 
 # Bitcoin Daemon Here - starts a bitcoind in current directory
 bdh() {
-  test -d "$1" && { MYDIR="$1"; shift; }
+  test -d "$1" && { cd "$1"; shift; }
   OPTS="$@"
   sh -se <<-EOF
-	test "$MYDIR" = "" || cd "$MYDIR"
-
 	test "\${PWD##*/}" = "signet" && chain=signet
 	test "\${PWD##*/}" = "testnet3" && chain=test
 	test "\${PWD##*/}" = "regtest" && chain=regtest
@@ -19,16 +17,44 @@ bdh() {
 
 # Bitcoin Client Here - starts a bitcoin-cli in current directory
 bch() {
-  test -d "$1" && { MYDIR="$1"; shift; }
+  test -d "$1" && { cd "$1"; shift; }
   OPTS="$@"
   sh -se <<-EOF
-	test "$MYDIR" = "" || cd "$MYDIR"
-
 	test "\${PWD##*/}" = "signet" && chain=signet
 	test "\${PWD##*/}" = "testnet3" && chain=test
 	test "\${PWD##*/}" = "regtest" && chain=regtest
 	test "\$chain" = "" || bd=\${PWD%/*}
 
 	exec bitcoin-cli "-datadir=\${bd:-\$PWD}" -chain=\${chain:-main} $OPTS
+	EOF
+}
+
+# Lightning Daemon Here - starts a lightningd in current directory
+ldh() {
+  test -d "$1" && { cd "$1"; shift; }
+  OPTS="$@"
+  sh -se <<-EOF
+	test "\${PWD##*/}" = "bitcoin" && net=bitcoin
+	test "\${PWD##*/}" = "testnet" && net=testnet
+	test "\${PWD##*/}" = "signet" && net=signet
+	test "\${PWD##*/}" = "regtest" && net=regtest
+
+	exec lightningd "--lightning-dir=\${PWD%/*}" "--network=\$net" $OPTS
+	EOF
+}
+
+# Lightning Client Here - starts a lightning-cli in current directory
+lch() {
+  test -d "$1" && { cd "$1"; shift; }
+  test -d bitcoin && cd bitcoin
+  OPTS="$@"
+  sh -se <<-EOF
+	test "\${PWD##*/}" = "bitcoin" && net=bitcoin
+	test "\${PWD##*/}" = "testnet" && net=testnet
+	test "\${PWD##*/}" = "signet" && net=signet
+	test "\${PWD##*/}" = "regtest" && net=regtest
+
+	exec timeout 600 \
+	  lightning-cli "--lightning-dir=\${PWD%/*}" "--network=\$net" $OPTS
 	EOF
 }
