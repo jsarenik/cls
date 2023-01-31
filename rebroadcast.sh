3596f3deea538017d72b57277cf4bd57eb8ad28a
Jan Sarenik <jajomojo@gmail.com>
2023-01-31 13:59:06 +0100

    rebroadcast.sh: shorten and simplify
    No mixing of comms, now using bitcoin-cli (via bch.sh)
    for everything. Can be called from any blocknotify script
    which already runs with the needed path for bch.sh.

--- rebroadcast.sh
+++ rebroadcast.sh
@@ -1,37 +1,27 @@
 #!/bin/sh
 
-export LC_ALL=C
-RCFILE=$HOME/.bitcoinrc
-test -r $RCFILE && . $RCFILE
-# set environment variable USER=user:pass there
+lock=/tmp/${0##*/}-$(echo ${PWD} | md5sum | cut -b-8)
+mkdir $lock || exit 1
 
-TMPL=/tmp/mempool$$
-trap "rm -rfv $TMPL*; exit 1" EXIT INT QUIT
+mempool=/tmp/mempool$$
+trap "rm -rfv $mempool; exit" INT QUIT
 
-printf "Preparing current mempool txid snapshot... "
+printf "%s " "Preparing current mempool txid snapshot..."
 bch.sh getrawmempool \
-  | sed '/^\[/d;/^\]/d;s/,$//;s/^  //;s/^"//;s/"$//' \
-  | split -l 256 -a 5 - $TMPL \
+  | tr -d '[], "' | sed '/^$/d' \
+  > $mempool \
   && echo done
 
-ls $TMPL* >/dev/null 2>&1 \
-  || { echo "Mempool empty. Sleeping."; sleep 60; exec $0; }
+ls $mempool >/dev/null 2>&1 \
+  || { echo "Mempool empty. Exiting."; exit; }
 
-printf "Broadcasting: "
-for file in $TMPL*; do
-{ printf '['
-  cat $file | while read tx
+echo Rebroadcasting transactions from the mempool...
+  while read tx
   do
     test -n "$tx" || continue
-    RAW=$(bch.sh getrawtransaction $tx 2>/dev/null) || continue
-    printf '{"jsonrpc": "1.0", "id": "send", "method": "sendrawtransaction", "params": ["%s"]},' $RAW
-  done
-  printf '{"jsonrpc": "1.0", "id": "getbc", "method": "getblockcount", "params": []}]\n'
-} | curl -s --user $USER --data-binary @- -H 'content-type: text/plain;' \
-    http://127.0.0.1:${1:-8332}/ >/dev/null \
-  && { rm $file && printf .; } \
-  || { echo "ERR $file"; exit 1; }
-done
-echo done
-sleep ${SLEEP:-20}
-exec $0 "$@"
+    { bch.sh getrawtransaction $tx | bch.sh -stdin sendrawtransaction 50; } \
+      >/dev/null 2>&1
+  done < $mempool
+  rm $mempool
+echo Rebroadcast done
+rmdir $lock
