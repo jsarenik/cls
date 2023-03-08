2de5d34e73a61aee1f108f262baf3a923ea16a2a
Jan Sarenik <jajomojo@gmail.com>
2023-03-08 15:17:31 +0100

    bch.sh: Add -rpcwallet if in wallet dir
    Example:
    
        cd ~/.bitcoin/signet/wallets/test
        bch.sh getbalance
        0.00000000
    
    Works also for wallets not in "wallets" directory.

--- bch.sh
+++ bch.sh
@@ -2,9 +2,12 @@
 
 test -d "$1" && { cd "$1"; shift; }
 
+test -r wallet.dat && { wallet="-rpcwallet=${PWD##*/}"; cd ..; }
+test "${PWD##*/}" = "wallets" && cd ..
+
 test "${PWD##*/}" = "signet" && chain=signet
 test "${PWD##*/}" = "testnet3" && chain=test
 test "${PWD##*/}" = "regtest" && chain=regtest
 test "$chain" = "" || ddir=${PWD%/*}
 
-exec bitcoin-cli "-datadir=${ddir:-$PWD}" -chain=${chain:-main} "$@"
+exec bitcoin-cli $wallet "-datadir=${ddir:-$PWD}" -chain=${chain:-main} "$@"
