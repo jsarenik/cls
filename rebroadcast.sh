0b05219658056a6d9d62a1935928e778399bc07d
Jan Sarenik <jajomojo@gmail.com>
2023-03-08 15:16:49 +0100

    rebroadcast.sh: fix xargs, forgotten commit

--- rebroadcast.sh
+++ rebroadcast.sh
@@ -7,7 +7,7 @@ mkdir $lock >/dev/null 2>&1 || exit 1
 echo Rebroadcasting transactions from the mempool...
 { bch.sh getrawmempool \
   | tr -d '[], "' | sed '/^$/d' \
-  | xargs -n1 bch.sh getrawtransaction \
+  | xargs -r -n1 bch.sh getrawtransaction \
   | bch.sh -stdin sendrawtransaction 50; } >/dev/null 2>&1
 echo Rebroadcast done
 rmdir $lock
