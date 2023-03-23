a38ff58f7ecb645859095a3b9bce80083371da1f
Jan Sarenik <jajomojo@gmail.com>
2023-03-23 07:41:54 +0100

    nicehash.sh: Rewrite I.

--- nicehash.sh
+++ nicehash.sh
@@ -1,28 +1,67 @@
 #!/bin/sh
 
-hash=${1:-$(wget -qO - http://ln.anyone.eu.org/getblockhash.txt)}
-count=${2:-$(wget -qO - http://ln.anyone.eu.org/getblockcount.txt)}
-tmp=$PREFIX/tmp/middle
+# backward compatible is when called with 2 arguments, <hash> <count>
+#  and also without any argument
+# new is <hash> only
+# super new is <count> only (number of chars is <64)
 
-cat > $tmp-left <<EOF
-       | ..  
-       | 1.  
-       | 2.  
-       | 3.  
+usage() {
+cat <<EOF
+Usage: nicehash.sh <hash> <count>
+    or nicehash.sh <hash>
+    or nicehash.sh <count>
+    or nicehash.sh
+
+Where the single <count> is < 64 digits long.
 EOF
-cat > $tmp-right <<EOF
-  .f |
-  1f |
-  2f |
-  3f |
-EOF
+  exit 1
+}
+
+gbhurl=http://ln.anyone.eu.org/getblockhash.txt
+api=http://beh.bublina.eu.org/api/block/header
+
+test $# -le 2 || usage
+test $# -ne 2 && {
+  test $# -eq 0 && {
+    hash=${1:-$(wget -qO - $gbhurl)}
+    count=$(wget -qO - $api/$hash \
+      | tr , '\n' | grep height | cut -d: -f2-)
+  } || {
+  if
+    test $(echo $1 | wc -c) -lt 64
+  then
+    count=$1
+    hash=$(wget -qO - $api/$count \
+      | tr , '\n' | grep hash | cut -d: -f2- | tr -d '"')
+  else
+    hash=$1
+    count=$(wget -qO - $api/$hash \
+      | tr , '\n' | grep height | cut -d: -f2-)
+  fi
+  }
+} || {
+  hash=$1
+  count=$2
+}
+
+from=0
+to=.
 
 printblock.sh $count
-echo "       ,---   .123 4567 89ab cdef   ---,"
-#echo '       |###   #### #### #### ####   ###|'
 echo $hash \
   | sed -E 's/.{16}/&\n/g' \
   | sed '$d; s/..../& /g; s/ $//' \
-  | sed 's/0/./g' \
-  | paste -d " " $tmp-left - $tmp-right
-echo "       '===   ==== ==== ==== ====   ==='"
+  | {
+  read line1
+  read line2
+  read line3
+  read line4
+cat << EOF
+       ,---   .123 4567 89ab cdef   ---,
+       | ..   $line1   .f |
+       | 1.   $line2   1f |
+       | 2.   $line3   2f |
+       | 3.   $line4   3f |
+       '===   ==== ==== ==== ====   ==='
+EOF
+} | tr "$from" "$to"
