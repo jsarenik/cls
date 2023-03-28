44d6fe0721e88f359e31fa653653838045b36c67
Jan Sarenik <jajomojo@gmail.com>
2023-03-28 11:37:01 +0200

    bfunky: Safe environment with braces
    Instead of curly braces.
    Also simplification and generalization.

--- bfunky.sh
+++ bfunky.sh
@@ -1,80 +1,60 @@
 #!/bin/sh -c "echo source me"
 # source this file or embed it in your script
 
-# Bitcoin Daemon Here - starts a bitcoind in current directory
-bdh() {
-  test -d .bitcoin && cd .bitcoin
-  test -d "$1" && { cd "$1"; shift; }
-  OPTS="$@"
-  sh -se <<-EOF
-	test "${PWD##*/}" = "signet" && chain=signet
-	test "${PWD##*/}" = "testnet3" && chain=test
-	test "${PWD##*/}" = "regtest" && chain=regtest
-	test "\$chain" = "" || bd=${PWD%/*}
-
-	exec bitcoind "-datadir=\${bd:-$PWD}" -chain=\${chain:-main} $OPTS
-	EOF
+setbchain() {
+  test "${PWD##*/}" = "signet" && chain=signet
+  test "${PWD##*/}" = "testnet3" && chain=test
+  test "${PWD##*/}" = "regtest" && chain=regtest
+  test "$chain" = "" || ddir=${PWD%/*}
 }
 
+# Bitcoin Daemon Here - starts a bitcoind in current directory
+bdh() (
+  test -d .bitcoin && cd .bitcoin
+  test -d "$1" && { cd "$1"; shift; }
+  setbchain
+  bitcoind "-datadir=${ddir:-$PWD}" -chain=${chain:-main} "$@"
+)
+
 # Bitcoin Client Here - starts a bitcoin-cli in current directory
-bch() {
-  unset wallet
+bch() (
   test -d .bitcoin && cd .bitcoin
   test -d "$1" && { cd "$1"; shift; }
   test -r wallet.dat && { wallet="-rpcwallet=${PWD##*/}"; cd ..; }
-  test -r wosh.cat && cd ..
+  echo $PWD | grep -q wosh- && test -r wosh.cat && cd ..
   test "${PWD##*/}" = "wallets" && cd ..
-  OPTS="$@"
-  sh -se <<-EOF
-	test "${PWD##*/}" = "signet" && chain=signet
-	test "${PWD##*/}" = "testnet3" && chain=test
-	test "${PWD##*/}" = "regtest" && chain=regtest
-	test "\$chain" = "" || bd=${PWD%/*}
+  setbchain
+  bitcoin-cli $wallet "-datadir=${ddir:-$PWD}" \
+    -chain=${chain:-main} "$@"
+)
 
-	exec bitcoin-cli $wallet "-datadir=\${bd:-$PWD}" \
-	  -chain=\${chain:-main} $OPTS
-	EOF
+setlnet() {
+  test "${PWD##*/}" = "bitcoin" && net=bitcoin
+  test "${PWD##*/}" = "testnet" && net=testnet
+  test "${PWD##*/}" = "signet" && net=signet
+  test "${PWD##*/}" = "regtest" && net=regtest
 }
 
 # Lightning Daemon Here - starts a lightningd in current directory
-ldh() {
+ldh() (
   test -d "$1" && { cd "$1"; shift; }
-  OPTS="$@"
-  sh -se <<-EOF
-	test "${PWD##*/}" = "bitcoin" && net=bitcoin
-	test "${PWD##*/}" = "testnet" && net=testnet
-	test "${PWD##*/}" = "signet" && net=signet
-	test "${PWD##*/}" = "regtest" && net=regtest
-
-	exec lightningd "--lightning-dir=${PWD%/*}" "--network=\$net" $OPTS
-	EOF
-}
+  setlnet
+  lightningd "--lightning-dir=${PWD%/*}" "--network=$net" "$@"
+)
 
 # Lightning Client Here - starts a lightning-cli in current directory
-lch() {
+lch() (
   test -d "$1" && { cd "$1"; shift; }
   test -d bitcoin && cd bitcoin
-  OPTS="$@"
-  sh -se <<-EOF
-	test "${PWD##*/}" = "bitcoin" && net=bitcoin
-	test "${PWD##*/}" = "testnet" && net=testnet
-	test "${PWD##*/}" = "signet" && net=signet
-	test "${PWD##*/}" = "regtest" && net=regtest
+  setlnet
+  lightning-cli "--lightning-dir=${PWD%/*}" "--network=$net" "$@"
+)
 
-	exec timeout 600 \
-	  lightning-cli "--lightning-dir=${PWD%/*}" "--network=\$net" $OPTS
-	EOF
-}
-
-# Bitcoin Tool Here - starts bitcointool with network
-bth() {
+# Bitcoin Tool Here - starts bitcointool with network parameter
+bth() (
   test -d "$1" && { cd "$1"; shift; }
-  OPTS="$@"
-  sh -se <<-EOF
-	test "${PWD##*/}" = "signet" && chain=-t
-	test "${PWD##*/}" = "testnet3" && chain=-t
-	test "${PWD##*/}" = "regtest" && chain=-r
-
-	exec bitcointool \${chain} $OPTS
-	EOF
-}
+  test "${PWD##*/}" = "signet" && chain=-t
+  test "${PWD##*/}" = "testnet3" && chain=-t
+  test "${PWD##*/}" = "regtest" && chain=-r
+  bitcointool ${chain} "$@"
+)
