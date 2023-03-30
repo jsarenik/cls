0524ea9400a814bac9ae1fe57410e0342c4457d1
Jan Sarenik <jajomojo@gmail.com>
2023-03-30 10:21:05 +0200

    bth.sh: Fix for wosh wallets

--- bth.sh
+++ bth.sh
@@ -2,6 +2,9 @@
 
 test -d "$1" && { cd "$1"; shift; }
 
+echo $PWD | grep -q wosh- && cd ..
+test "${PWD##*/}" = "wallets" && cd ..
+
 test "${PWD##*/}" = "signet" && chain=-t
 test "${PWD##*/}" = "testnet3" && chain=-t
 test "${PWD##*/}" = "regtest" && chain=-r
