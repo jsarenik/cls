f53b27ea5932536f2b6d68e684b15897a172ecce
Jan Sarenik <jajomojo@gmail.com>
2023-03-31 10:38:16 +0200

    Add printblock.sh
    This was missing and used in other scripts :)
    Uses figlet(6), wc(1), grep(1), sed(1), printf(1)
    and bch.sh from this repository if started without
    an argument which would stand for the number to print.
    
    Example:
    
        $ printblock.sh 123
                      __    __           __
                     / /_  / /___  _____/ /__
                    / __ \/ / __ \/ ___/ //_/
                   / /_/ / / /_/ / /__/ ,<
                  /_.___/_/\____/\___/_/|_|
    
                           ______  _____
                          <  /__ \|__  /
                          / /__/ / /_ <
                         / // __/___/ /
                        /_//____/____/

---
+++ printblock.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+test "$1" = "-d" && { debug=1; shift; }
+test "$1" = "" && exit
+bc=${1:-$(bch.sh getblockcount)}
+fontdir=${PREFIX:-/usr}/share/figlet/fonts
+test -d $fontdir || fontdir=${fontdir%/*}
+
+echo $1 | grep -q '^[0-9]\+$' && { bc=$1; shift; }
+font=$(printf "banner\nbig\nblock\nscript\nsmall\nsmshadow\nslant\nsmslant\nstandard\n" | sed -n "$(($bc%9+1))p")
+
+thousands() {
+  sed -r ':L;s=\b([0-9]+)([0-9]{3})\b=\1 \2=g;t L'
+}
+bcnew=$bc
+test $(printf "$bc" | wc -c) -gt 6 \
+  && bcnew=$(printf "$bc" | thousands)
+test "$font" = "block" -o "$font" = "banner" \
+  && bcnew=$(printf "$bc" | thousands) \
+  || bcnew="block$bcnew"
+
+{
+figlet -w 48 -c -d $fontdir -f $font \
+  "$bcnew"
+} | sed 's/\s\+$//' | uniq
+
+test "$debug" = "1" -o "$1" = "-d" && echo $font
