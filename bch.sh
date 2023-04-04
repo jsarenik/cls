2477def2ffd84048cca5bb81e068c7844793eedb
Jan Sarenik <jajomojo@gmail.com>
2023-04-04 14:22:43 +0200

    bch.sh: Separate the whole wallet-handling block
    bch.sh is now able to do arbitraty-depth magic.
    Example:
    
        $ cd .bitcoin/wallets/specter_id
        $ cd hunting
        $ pwd
        $HOME/.bitcoin/wallets/specter_id/sats
        $ bch.sh loadwallet
        {
          "name": "specter_id/sats",
          "warning": ""
        }
        $ bch.sh getbalance
        0.00001234
        $ bch.sh unloadwallet
        {
          "warning": ""
        }
    
    Or allows automated magic like
    
        $ cd .bitcoin/wallets/specter_id
        $ loadaun() { bch.sh loadwallet; bch.sh unloadwallet; }
        $ for w in *; do cd $w; loadaun; cd ..; done
        {
          "name": "specter_id/stack",
          "warning": ""
        }
        {
          "warning": ""
        }
        {
          "name": "specter_id/sats",
          "warning": ""
        }
        {
          "warning": ""
        }
    
    ... to refresh old wallets on a full node.

--- bch.sh
+++ bch.sh
@@ -3,13 +3,20 @@
 test -d .bitcoin && cd .bitcoin
 test -d "$1" && { cd "$1"; shift; }
 
-test -r wallet.dat && { wallet="-rpcwallet=${PWD##*/}"; cd ..; }
-test -r wosh.cat && cd ..
-test "${PWD##*/}" = "wallets" && cd ..
+# Handling of inside-the-wallet-dir cases
+test -r wallet.dat -o -r wosh.cat && {
+  mypwd=$PWD
+  echo $PWD | grep -q '/wallets' \
+    && until test "${PWD##*/}" = "wallets"; do cd ..; done
+  wn=${mypwd##$PWD/}
+  w="-rpcwallet=${wn}"
+  test "$1" = "loadwallet" && add="$wn"
+  cd ..
+}
 
 test "${PWD##*/}" = "signet" && chain=signet
 test "${PWD##*/}" = "testnet3" && chain=test
 test "${PWD##*/}" = "regtest" && chain=regtest
 test "$chain" = "" || ddir=${PWD%/*}
 
-exec bitcoin-cli $wallet "-datadir=${ddir:-$PWD}" -chain=${chain:-main} "$@"
+exec bitcoin-cli $w -datadir=${ddir:-$PWD} -chain=${chain:-main} "$@" $add
