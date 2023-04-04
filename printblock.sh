22a96eb7f12c4f3d89b263b15e6caf5fcc5369b8
Jan Sarenik <jajomojo@gmail.com>
2023-04-04 14:20:21 +0200

    printblock.sh: Make BSD-compatible
    Tested on OpenBSD 7.1 and NetBSD 9.3 (of sdf.org)
    
    Also fixes the return value in case of -d.

--- printblock.sh
+++ printblock.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+type figlet >/dev/null 2>&1 || exit 1
 test "$1" = "-d" && { debug=1; shift; }
 test "$1" = "" && exit
 bc=${1:-$(bch.sh getblockcount)}
@@ -7,8 +8,10 @@ bc=${1:-$(bch.sh getblockcount)}
 echo $1 | grep -q '^[0-9]\+$' && { bc=$1; shift; }
 font=$(printf "banner\nbig\nblock\nscript\nsmall\nsmshadow\nslant\nsmslant\nstandard\n" | sed -n "$(($bc%9+1))p")
 
+test "$debug" = "1" -o "$1" = "-d" && echo $font
+
 thousands() {
-  sed -r ':L;s=\b([0-9]+)([0-9]{3})\b=\1 \2=g;t L'
+  sed -r -e ':L' -e 's=([0-9]+)([0-9]{3})=\1 \2=g' -e 't L'
 }
 bcnew=$bc
 test $(printf "$bc" | wc -c) -gt 6 \
@@ -21,5 +24,3 @@ test "$font" = "block" -o "$font" = "banner" \
 figlet -w 48 -c -f $font \
   "$bcnew"
 } | sed 's/^[[:space:]]*$//g' | uniq
-
-test "$debug" = "1" -o "$1" = "-d" && echo $font
