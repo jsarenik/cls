53f32481d9cfacc402dbde1ef41c50600e24f808
Jan Sarenik <jajomojo@gmail.com>
2023-05-10 17:57:32 +0200

    nice*: Use bch.sh instead of wget

--- shortform.sh
+++ shortform.sh
@@ -5,7 +5,7 @@
 # new is <hash> only
 # super new is <count> only (number of chars is <64)
 
-VER=1.1.1
+VER=1.3.1
 type md5 >/dev/null 2>&1 && md5=md5
 VERSION=$VER-$(sed 1d $0 | ${md5:-"md5sum"} | cut -b-5)
 test "$1" = "-o" && { only=1; shift; }
@@ -25,28 +25,25 @@ thousands() {
   sed -r -e ':L' -e 's=([0-9]+)([0-9]{3})=\1,\2=g' -e 't L'
 }
 
-gbhurl=http://ln.anyone.eu.org/getblockhash.txt
-api=http://beh.bublina.eu.org/api/block/header
-
 test "$1" = "-V" && { echo $VERSION; exit; }
 test "$1" = "-h" && usage
 test $# -le 2 || usage
 test $# -lt 2 && {
   test $# -eq 0 && {
-    hash=${1:-$(wget -qO - $gbhurl)}
-    count=$(wget -qO - $api/$hash \
-      | tr , '\n' | grep height | cut -d: -f2-)
+    hash=${1:-$(bch.sh getbestblockhash)}
+    count=$(bch.sh getblockheader $hash \
+      | tr , '\n' | grep -w height | cut -d: -f2-)
   } || {
   if
     count=$(echo $1 | tr -cd '[0-9]')
     test "$count" = "$1"
   then
-    hash=$(wget -qO - $api/$count \
+    hash=$(bch.sh getblockheader $(bch.sh getblockhash $count) \
       | tr , '\n' | grep -w hash | cut -d: -f2- | tr -d '"')
   else
     hash=$1
-    count=$(wget -qO - $api/$hash \
-      | tr , '\n' | grep height | cut -d: -f2-)
+    count=$(bch.sh getblockheader $hash \
+      | tr , '\n' | grep -w height | cut -d: -f2-)
   fi
   }
 } || {
