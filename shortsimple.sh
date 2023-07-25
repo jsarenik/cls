fdb5bfccfd4013a26fd71a7bc7e4c38a9fa8649d
Jan Sarenik <jajomojo@gmail.com>
2023-07-25 17:07:40 +0200

    niceblack: Use shortsimple.sh

--- shortsimple.sh
+++ shortsimple.sh
@@ -2,6 +2,7 @@
 
 BH=${1:-$(bch.sh getbestblockhash)}
 last=$(echo $BH | cut -b61-64)
-a=$(echo $BH | fold -w 4 | grep -Ev '^(0000|[1-9a-f]{4})$' | tr "0\n" ". ")
-R=$(echo $a $last | cut -b-20 )
-echo $R | grep "$last " || echo $R M
+a=$(echo $BH | cut -b-60 | fold -w 4 | grep -Ev '^(0000|[1-9a-f]{4})$')
+R=$(echo $a $last | cut -b-20)
+{ echo $R | grep "$last$" || echo $R M; } | tr "0\n" ". "
+echo
