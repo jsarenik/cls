4183a263f37c0030c60efc03191006aeeab4c405
Jan Sarenik <jajomojo@gmail.com>
2023-07-26 05:17:21 +0200

    shortsimple.sh: Simplify grep

--- shortsimple.sh
+++ shortsimple.sh
@@ -2,7 +2,7 @@
 
 BH=${1:-$(bch.sh getbestblockhash)}
 last=$(echo $BH | cut -b61-64)
-a=$(echo $BH | cut -b-60 | fold -w 4 | grep -Ev '^(0000|[1-9a-f]{4})$')
+a=$(echo $BH | cut -b-60 | fold -w 4 | grep -Ev '^(0000|[^0]{4})$')
 R=$(echo $a $last | cut -b-20)
 { echo $R | grep "$last$" || echo $R M; } | tr "0\n" ". "
 echo
