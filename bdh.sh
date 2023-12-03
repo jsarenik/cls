5b8df03405cfd1b59fd3280e0d2d7be9d56ef18a
Jan Sarenik <jajomojo@gmail.com>
2023-12-03 10:38:46 +0000

    bdh.sh: shorten exec line, add two more setting ddir

--- bdh.sh
+++ bdh.sh
@@ -6,6 +6,7 @@ test -d "$1" && { cd "$1"; shift; }
 test "${PWD##*/}" = "signet" && chain=signet
 test "${PWD##*/}" = "testnet3" && chain=test
 test "${PWD##*/}" = "regtest" && chain=regtest
-test "$chain" = "" || ddir=${PWD%/*}
+ddir=$PWD
+ddir=${ddir:-$PWD%/*}
 
-exec bitcoind "-datadir=${ddir:-$PWD}" -chain=${chain:-main} "$@"
+exec bitcoind "-datadir=$ddir" -chain=${chain:-main} "$@"
