8f8ad7a93ba69fd612a1e68bb9fda5a8d3c6510a
Jan Sarenik <jajomojo@gmail.com>
2024-07-29 14:56:34 +0200

    Update bdh.sh for multiple dev playgrounds
    Default is Knots
    Also supports:
      - Libre Relay (main)
      - Inquisition (signet)
      - etc.

--- bdh.sh
+++ bdh.sh
@@ -1,13 +1,23 @@
 #!/bin/sh
 
+test -d "$1" && { cd "$1"; shift; }
 test -d .bitcoin && cd .bitcoin
 export MALLOC_ARENA_MAX=1
 
-test -d "$1" && { cd "$1"; shift; }
+mypwd=$PWD
+test -L $mypwd && mypwd=$(readlink $mypwd)
+echo $mypwd | grep -q "^/" || mypwd="../$mypwd"
+cd $mypwd
 
-test "${PWD##*/}" = "signet" && chain=signet
+test "${PWD##*/}" = "signet" && chain=signet \
+  PATH=$HOME/bin/bitcoin-27.0-inq/bin:$PATH
 test "${PWD##*/}" = "testnet3" && chain=test
+test "${PWD##*/}" = "testnet4" && chain=testnet4 \
+  PATH=$HOME/bin/bitcoin-testnet4/bin:$PATH
 test "${PWD##*/}" = "regtest" && chain=regtest
-test "$chain" = "" || ddir=${PWD%/*}
 
-exec bitcoind "-datadir=${ddir:-$PWD}" -chain=${chain:-main} "$@"
+test "$chain" = "" \
+    && PATH=$HOME/bin/bitcoin-knots/bin:$PATH \
+    || { ddir=${PWD%/*}; chain="-chain=$chain"; }
+
+exec bitcoind "-datadir=${ddir:-$PWD}" ${chain} "$@"
