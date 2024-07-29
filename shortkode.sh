ade0842d6a7c54c8eb4df7cabb1a3cfa7b26669e
Jan Sarenik <jajomojo@gmail.com>
2024-07-29 13:19:20 +0200

    shortkode.sh: Use shell to cut

--- shortkode.sh
+++ shortkode.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 BH=${1:-$(bch.sh getbestblockhash)}
-last=$(echo $BH | cut -b61-64)
-nz=$(echo $BH | fold -w 4 | grep -E '^[^0]{4}$' | wc -l)
-z=$(echo $BH | fold -w 4 | grep '^0000$' | wc -l)
-printf "%s %x%x" $last ${nz} ${z} | tr "0\n" ". "
+last=${BH#${BH%????}}
+nz=$(echo $BH | fold -w 4 | grep -cE '^[^0]{4}$')
+z=$(echo $BH | fold -w 4 | grep -c '^0000$')
+printf "%s %x" $last \
+  "$(((${nz} <<4) + ${z}))" \
+  | tr "0\n" ". "
 echo
