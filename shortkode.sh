f0bf0e9ed3d8cd89e4e4708ee0a1c57fdec8358f
Jan Sarenik <jajomojo@gmail.com>
2024-07-29 13:47:38 +0200

    shortkode.sh: Add -b and -h options
    Examples:
    
        $ shortkode.sh -b
        854486 22.6 94
        $ shortkode.sh -h
        0d09d622.694
        $ shortkode.sh -h | ce.sh
        94.622d6090d

--- shortkode.sh
+++ shortkode.sh
@@ -1,10 +1,21 @@
 #!/bin/sh
+. /dev/shm/UpdateTip-bitcoin
+d=" "
+if
+  test "$1" = "-b" -o "$1" = "-h"
+then
+  BC=${height:-$(bch.sh getblockcount)}
+  BH=${best:-$(bch.sh getblockhash $BC)}
+  test "$1" = "-b" && printf "%s$d" "$BC" \
+  || { d=""; l=$(printf "%x" $BC | wc -c); lh=$((l+l%2)); printf "%0${lh}x" "$BC"; }
+else
+  BH=${1:-${best:-(bch.sh getbestblockhash)}}
+fi
 
-BH=${1:-$(bch.sh getbestblockhash)}
 last=${BH#${BH%????}}
+
 nz=$(echo $BH | fold -w 4 | grep -cE '^[^0]{4}$')
 z=$(echo $BH | fold -w 4 | grep -c '^0000$')
-printf "%s %x" $last \
+printf "%s$d%x\n" "$last" \
   "$(((${nz} <<4) + ${z}))" \
-  | tr "0\n" ". "
-echo
+  | tr 0 .
