cd80194589ea3fbbff6da6564647087995ef75a2
Jan Sarenik <jajomojo@gmail.com>
2025-01-01 09:27:38 +0000

    bch: Simplify and also lch-compat

--- bch.sh
+++ bch.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-#rpc decoderawtransaction drt
-#rpc signrawtransactionwithwallet srt
-#rpc sendrawtransaction sert
-#rpc loadwallet lw
-#rpc getbalances gebas
-#rpc getblockcount gbc
-
 test -d "$1" && { cd "$1"; shift; }
 test -d .bitcoin && cd .bitcoin
 ls | grep -q . || exit 1
@@ -14,15 +7,10 @@ ls | grep -q . || exit 1
 # Handling of inside-the-wallet-dir cases
 test -r wallet.dat && {
   mypwd=$PWD
-  cd ..
-  echo $mypwd | grep -qw wosh || {
-    test -L $mypwd && mypwd=$(readlink $mypwd)
-    wn=${mypwd##$PWD/}
-    w="-rpcwallet=${wn}"
-    test "$1" = "loadwallet" && add="$wn"
-  }
+  test -L $PWD && mypwd=$(readlink $PWD)
+  w="-rpcwallet=${mypwd##*/}"
 }
-test "${PWD##*/}" = "wallets" && cd ..
+cd ${PWD%%/wallets*}
 
 test -L $PWD && {
   mypwd=$(readlink $PWD)
@@ -33,16 +21,16 @@ test -L $PWD && {
 
 cmd=bitcoin-cli
 
+test -r blocks || cd ~/.bitcoin
+
 # main signet testnet3 testnet4 regtest liquidv1 liquidtestnet liquidregtest
 c="${PWD##*/}"; chain=${c%net3}
+echo $chain | grep -qE '^(signet|test|testnet4|regtest|liquid)' \
+	&& ddir=${PWD%/*} || chain=""
 
-test "${chain}" = "signet" && PATH=$HOME/bin/bitcoin-27.0-inq/bin:$PATH
-test "${chain}" = "testnet4" && PATH=$HOME/bin/bitcoin-testnet4/bin:$PATH
 test "${chain%${chain#liquid}}" = "liquid" && cmd=elements-cli
-ddir=${PWD%/*}
-test "$chain" = ".bitcoin" && { ddir=$PWD; chain=""; }
 
 test "$1" = "-k" && exec pkill -f "$cmd -datadir=${ddir:-$PWD}"
 test "$1" = "-g" && exec pgrep -f "$cmd -datadir=${ddir:-$PWD}"
 
-exec $cmd -datadir=${ddir:-$PWD} -chain=${chain:-main} $w "$@" $add
+exec $cmd -rpcclienttimeout=0 -datadir=${ddir:-$PWD} -chain=${chain:-main} $w "$@"
