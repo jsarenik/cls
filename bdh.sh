55a3ad4df5d265e25b5b31221d57c1936ae7a1a7
Jan Sarenik <jajomojo@gmail.com>
2025-01-01 09:41:56 +0000

    bdh: Update and simptlify. TIMTOWTDI
    The current stable Bitcoin Core v28.0 contains also testnet4.

--- bdh.sh
+++ bdh.sh
@@ -9,14 +9,10 @@ test -L $mypwd && mypwd=$(readlink $mypwd)
 echo $mypwd | grep -q "^/" || mypwd="../$mypwd"
 cd $mypwd
 
-test "${PWD##*/}" = "signet" && chain=signet \
-  PATH=$HOME/bin/bitcoin-27.0-inq/bin:$PATH
+test "${PWD##*/}" = "signet" && chain=signet
 test "${PWD##*/}" = "testnet3" && chain=test
-test "${PWD##*/}" = "testnet4" && chain=testnet4 \
-  PATH=$HOME/bin/bitcoin-testnet4/bin:$PATH
+test "${PWD##*/}" = "testnet4" && chain=testnet4
 test "${PWD##*/}" = "regtest" && chain=regtest
+test "$chain" = "" || ddir=${PWD%/*}
 
-test "$chain" = "" \
-    || { ddir=${PWD%/*}; chain="-chain=$chain"; }
-
-exec bitcoind "-datadir=${ddir:-$PWD}" ${chain} "$@"
+exec bitcoind "-datadir=${ddir:-$PWD}" -chain=${chain:-main} "$@"
