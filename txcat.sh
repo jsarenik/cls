e842dfbace4ec5a55c6b515de5083c2956b062d8
Jan Sarenik <jsarenik@email.cz>
2025-11-26 08:16:03 +0100

    txcat.sh: comments anywhere on line now

--- txcat.sh
+++ txcat.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # This is txcat.sh, a simple script that
 # prints a bare raw transaction on line
-grep -v '^#' $1 | tr -cd '[0-9a-f]' | grep .
+sed -E 's/#[^ ]+//g' $1 | tr -cd '[0-9a-f]' | grep .
